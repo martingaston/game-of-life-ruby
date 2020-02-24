f6585a9ce6043ec434921db6e9e901b3b23b514c
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2020-02-24 22:14:56 +0000

    add game window and colours

---
+++ lib/colours.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'gosu'
+
+class Colours
+  LIVE_CELL = Gosu::Color.rgba(99, 29, 118, 255)
+  DEAD_CELL = Gosu::Color.rgba(46, 37, 50, 255)
+  WHITE = Gosu::Color.rgba(251, 251, 251, 255)
+  BLACK = Gosu::Color.rgba(32, 26, 35, 255)
+end
--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -1,19 +1,40 @@
+# frozen_string_literal: true
+
 require 'gosu'
+require_relative 'colours'
 
 class GameOfLife < Gosu::Window
+  WIDTH = 1024
+  HEIGHT = 768
+  CELL_SIZE = 8
+
   def initialize
-    super 1024, 768
-    self.caption = "Game Of Life"
+    super WIDTH, HEIGHT
+    self.caption = 'Game Of Life'
   end
 
   def needs_cursor?
     true
   end
 
-  def update
-  end
+  def update; end
 
   def draw
+    draw_background
+
+    (0..HEIGHT / CELL_SIZE).each do |y|
+      (0..WIDTH / CELL_SIZE).each do |x|
+        draw_cell(x, y)
+      end
+    end
+  end
+
+  def draw_background
+    Gosu.draw_rect 0, 0, width, height, Colours::BLACK
+  end
+
+  def draw_cell(x, y)
+    Gosu.draw_rect x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE - 1, CELL_SIZE - 1, Colours::DEAD_CELL
   end
 end
 
