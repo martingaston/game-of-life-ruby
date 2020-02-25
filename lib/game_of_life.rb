1e4168ea310bbb10109197f7425fe8a465fdd049
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2020-02-25 11:38:15 +0000

    render squares based on them being alive or dead

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -3,11 +3,15 @@
 require 'gosu'
 require_relative 'colours'
 require_relative 'config'
+require_relative 'world'
+require_relative 'location'
 
 class GameOfLife < Gosu::Window
   def initialize
     super Config::WIDTH, Config::HEIGHT
     self.caption = 'Game Of Life'
+    @world = World.empty
+    @world.add_at(Location.new(1, 1))
   end
 
   def needs_cursor?
@@ -18,6 +22,7 @@ class GameOfLife < Gosu::Window
 
   def draw
     draw_background
+    puts mouse_x
 
     (0..Config::HEIGHT / Config::CELL_SIZE).each do |y|
       (0..Config::WIDTH / Config::CELL_SIZE).each do |x|
@@ -34,7 +39,17 @@ class GameOfLife < Gosu::Window
     size = Config::CELL_SIZE - 1
     start_x = x * Config::CELL_SIZE
     start_y = y * Config::CELL_SIZE
-    Gosu.draw_rect start_x, start_y, size, size, Colours::DEAD_CELL
+    colour = get_cell_colour(x, y)
+    Gosu.draw_rect start_x, start_y, size, size, colour
+  end
+
+  def get_cell_colour(x, y)
+    location = Location.new(x, y)
+    if @world.alive_at?(location)
+      Colours::LIVE_CELL
+    else
+      Colours::DEAD_CELL
+    end
   end
 end
 
