4f2528a2c440af6775702246aa8e626551ca0aec
martingaston <35241716+martingaston@users.noreply.github.com>
2020-02-26 23:13:17 +0000

    add new config defaults for resolution and cell size

--- lib/config.rb
+++ lib/config.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Config
-  WIDTH = 320
-  HEIGHT = 240
-  CELL_SIZE = 8
+  WIDTH = 1024
+  HEIGHT = 768
+  CELL_SIZE = 16
   CELLS_WIDTH = WIDTH / CELL_SIZE
   CELLS_HEIGHT = HEIGHT / CELL_SIZE
   TICK_SPEED_MS = 125
